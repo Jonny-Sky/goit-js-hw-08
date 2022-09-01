203e8626924b8cc1f13561b322d94fb42d468152
Jonny-Sky <99253125+Jonny-Sky@users.noreply.github.com>
2022-09-01 11:48:21 +0300

    закінчує всі виправлення.

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,7 @@
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
+let formData = {};
 
 const form = document.querySelector('form');
 const textarea = document.querySelector('form textarea');
@@ -20,19 +20,6 @@ function messageSubmit() {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
-function onFormSubmit(evt) {
-  evt.preventDefault();
-  try {
-    const parsObjekt = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    console.log('Objekt:', parsObjekt);
-  } catch (error) {
-    console.log(error.message);
-  }
-
-  evt.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
-}
-
 function populateTextarea() {
   const savedMessage = localStorage.getItem(STORAGE_KEY);
   if (savedMessage) {
@@ -44,4 +31,17 @@ function populateTextarea() {
       textarea.value = message.message;
     }
   }
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  try {
+    const parsObjekt = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    console.log('Objekt:', parsObjekt);
+  } catch (error) {
+    console.log(error.message);
+  }
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  return (formData = {});
 }
